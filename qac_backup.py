9aea86510671a7563ee1bc70c06f40db4ce863b8
Kenneth Weigel <kennyweigel@gmail.com>
2013-08-16 15:55:09 -0400

    refactored

--- qac_backup.py
+++ qac_backup.py
@@ -2,7 +2,7 @@ import csv
 import os
 import re
 
-#appends newText to file at location: path
+#appends newText to file at location path
 def fileAppend(path, newText):
     dataSource = open(path, 'a')
     dataSource.write(newText)
@@ -17,48 +17,70 @@ print 'This program will only work if this script and exported CSV file are\n'+\
       'names created to 60 chars.  This means that the absolute path to\n'    +\
       'the directory that contains this script must be shorter than 60ish\n'  +\
       'chars.\n'
-raw_input('press enter to continue otherwise close window to exit...')
-print '\nRunning...'
 
-exportCSV = open('TestCases.csv','rb')  #opens CSV file
-dataCSV = csv.reader(exportCSV)         #parses CSV
-i = 0                                   #only execute first if statement once
+raw_input('press enter to continue otherwise close window to exit...\n')
+print 'Running...'
+
+exportCSV = open('TestCases.csv','rb')  
+#parses CSV
+dataCSV = csv.reader(exportCSV)         
+#only execute first 'if' statement once
+i = 0                                   
+
 for r in dataCSV:
-        
-    if not i:                           #only execute on the first line
+
+    #only execute on the first line   
+    if not i:                           
         testHead = str(r)[1:-1]     
         testHead = re.sub("[']","",testHead)
         for cN in range(0,len(r)):
             if r[cN] == 'Folder Name':
                 folderCol = cN
         i = 1
-    else:                               #execute on all following lines of csv
-        path = r[folderCol]             #file path location
-        path = path[:120]               #only allow first 120 chars
-        path = path.strip()             #and strip spaces
-            
-        if path == '':                  #if no filename make directory 'No Path'
+
+    #execute on all following lines of csv
+    else:                               
+        #file path location
+        path = r[folderCol]
+        #only allow first 120 chars
+        path = path[:120]               
+        #strip spaces
+        path = path.strip()             
+
+        #if no filename make directory 'No Path'
+        if path == '':
             path = 'No Path'
             if not os.path.exists(path): 
                 os.makedirs(path)
-        else:                           #if file name exists, make new directory
+
+        #if file name exists, make new directory
+        else:
             if not os.path.exists(path):
                 os.makedirs(path)
 
-        testContent = ''                #initialize testContent
-        for c in r:                     #combine all columns in row to 1 string
+        #initialize testContent
+        testContent = ''
+        #combine all columns in row to 1 string
+        for c in r:
             testContent = testContent+'"'+c+'",'
         
-        testContent = testContent[:-1]  #strip off last char ','
-        j = path.rfind('/')             #find last '/' in path
-        name = path[j+1:]               #gets lastchild of folder name, 
-        name = name[:60]                #only allow first 60 chars
-        name = name.strip()             #strips spaces
-        name = '/'+name+'.csv'          #adds '/' and '.csv'
-        path = path + name              #combines folder name and file name
+        #strip off last char ','
+        testContent = testContent[:-1]
+        #gets lastchild of folder name,
+        j = path.rfind('/')             
+        name = path[j+1:]                
+        #only allow first 60 chars
+        name = name[:60]
+        #strips spaces
+        name = name.strip()
+        #adds '/' and '.csv'
+        name = '/'+name+'.csv'
+        #combines folder name and file name
+        path = path + name
         
-        if not os.path.isfile(path):    #if <last_child>.csv doesn't exist
-            fileAppend(path,testHead)   #write column titles and nl
+        #if <last_child>.csv doesn't exist write column titles and nl
+        if not os.path.isfile(path):
+            fileAppend(path,testHead)
         testCase = '\n'+testContent
         fileAppend(path,testCase)
 
